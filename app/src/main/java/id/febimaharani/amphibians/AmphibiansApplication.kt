ff371d22dbcd215d40de7d7d0c767a0fe98b857d
FebiMaharani <147477650+FebiMaharani@users.noreply.github.com>
2024-12-13 00:04:27 +0700

    Update AmphibiansApplication.kt

--- app/src/main/java/id/febimaharani/amphibians/AmphibiansApplication.kt
+++ app/src/main/java/id/febimaharani/amphibians/AmphibiansApplication.kt
@@ -4,11 +4,13 @@ import android.app.Application
 import id.febimaharani.amphibians.data.AppContainer
 import id.febimaharani.amphibians.data.DefaultAppContainer
 
+// kelas ini berfungsi sebagai titik masuk aplikasi dan mengatur dependency injection
 class AmphibiansApplication : Application() {
-    /** AppContainer instance used by the rest of classes to obtain dependencies */
+    // instance AppContainer yang digunakan oleh kelas lain untuk mendapat dependensi
+    // menyediakan aksess kke repository lain.
     lateinit var container: AppContainer
     override fun onCreate() {
-        super.onCreate()
-        container = DefaultAppContainer()
+        super.onCreate() // menginisialisasi instance AppContainer untuk dependency injection
+        container = DefaultAppContainer() // menginisialisasi container dengan implementasi DefaultAppContainer
     }
 }
