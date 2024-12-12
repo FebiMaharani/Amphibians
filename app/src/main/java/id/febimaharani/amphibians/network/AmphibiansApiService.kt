b8370f39d08046eaa436bc6043186c0983aab118
FebiMaharani <147477650+FebiMaharani@users.noreply.github.com>
2024-12-12 23:35:03 +0700

    Update AmphibiansApiService.kt

--- app/src/main/java/id/febimaharani/amphibians/network/AmphibiansApiService.kt
+++ app/src/main/java/id/febimaharani/amphibians/network/AmphibiansApiService.kt
@@ -4,6 +4,6 @@ import id.febimaharani.amphibians.model.Amphibian
 import retrofit2.http.GET
 
 interface AmphibiansApiService {
-    @GET("amphibians")
-    suspend fun getAmphibians(): List<Amphibian>
+    @GET("amphibians") //menggunakan metode GET untuk mengambil data dari amphibians
+    suspend fun getAmphibians(): List<Amphibian> // mengembalikan daftar objek Amphibian
 }
